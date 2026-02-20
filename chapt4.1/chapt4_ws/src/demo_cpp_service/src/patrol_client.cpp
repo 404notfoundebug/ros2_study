a8c8b6c83521386f6be278b3ea15a08d0a1c78a0
Zhou <188126925@qq.com>
2026-02-20 17:54:46 +0800

    2月20

--- chapt4.1/chapt4_ws/src/demo_cpp_service/src/patrol_client.cpp
+++ chapt4.1/chapt4_ws/src/demo_cpp_service/src/patrol_client.cpp
@@ -2,59 +2,117 @@
 #include <chapt4_interfaces/srv/patrol.hpp>
 #include <cmath>
 #include <chrono>
-#include <ctime> 
+#include <ctime>
+#include <rcl_interfaces/msg/parameter.hpp>
+#include <rcl_interfaces/msg/parameter_value.hpp>
+#include <rcl_interfaces/msg/parameter_type.hpp>
+#include <rcl_interfaces/srv/set_parameters.hpp>
+
+using SetP = rcl_interfaces::srv::SetParameters;
 using Patrol = chapt4_interfaces::srv::Patrol;
 using namespace std::chrono_literals;
 
 class PatrolClient : public rclcpp::Node
 {
-    private:
-        rclcpp::TimerBase::SharedPtr timer_;
-        rclcpp::Client<Patrol>::SharedPtr patrol_client_;
+private:
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Client<Patrol>::SharedPtr patrol_client_;
 
-    public:
+public:
     PatrolClient() : rclcpp::Node("turtle_controler")
     {
-        srand(time(NULL));//初始化随机数种子
+        srand(time(NULL)); // 初始化随机数种子
         patrol_client_ = this->create_client<Patrol>("patrol");
-        timer_ = this->create_wall_timer(10s,[&]()->void
-    {
-        //1.检测服务端是否上线
-        while(!this->patrol_client_->wait_for_service(1s))
-        {
-            if(!rclcpp::ok())
-            {
-                RCLCPP_ERROR(this->get_logger(),"等待服务过程中，rclcpp挂了，我退下了");
-                return;
+
+        timer_ = this->create_wall_timer(10s, [this]() {
+            // 1.检测服务端是否上线
+            while (!this->patrol_client_->wait_for_service(1s)) {
+                if (!rclcpp::ok()) {
+                    RCLCPP_ERROR(this->get_logger(), "等待服务过程中，rclcpp挂了，我退下了");
+                    return;
+                }
+                RCLCPP_INFO(this->get_logger(), "等待服务上线中....");
             }
-            RCLCPP_INFO(this->get_logger(),"等待服务上线中....");
-        }
-        //2.构建请求对象
-        auto request = std::make_shared<Patrol::Request>();
-        request->target_x = rand() % 15;
-        request->target_y = rand() % 15;
-        RCLCPP_INFO(this->get_logger(),"准备好目标点%f,%f",request->target_x,request->target_y);
-        //3.发送请求
-        this->patrol_client_->async_send_request(request,[&](rclcpp::Client<Patrol>::SharedFuture result_future)->void{
-            auto response = result_future.get();
-            if(response->result==Patrol::Response::SUCCESS)
-            {
-                RCLCPP_INFO(this->get_logger(),"请求巡逻目标点成功");
-            }
-            if(response->result==Patrol::Response::FAIL)
-            {
-                RCLCPP_INFO(this->get_logger(),"请求巡逻目标点失败");
-            }
+
+            // 2.构建请求对象
+            auto request = std::make_shared<Patrol::Request>();
+            request->target_x = rand() % 15;
+            request->target_y = rand() % 15;
+            RCLCPP_INFO(this->get_logger(), "准备好目标点%f,%f", request->target_x, request->target_y);
+
+            // 3.发送请求
+            this->patrol_client_->async_send_request(
+                request,
+                [this](rclcpp::Client<Patrol>::SharedFuture result_future) {
+                    auto response = result_future.get();
+                    if (response->result == Patrol::Response::SUCCESS) {
+                        RCLCPP_INFO(this->get_logger(), "请求巡逻目标点成功");
+                    }
+                    if (response->result == Patrol::Response::FAIL) {
+                        RCLCPP_INFO(this->get_logger(), "请求巡逻目标点失败");
+                    }
+                });
         });
-    });
+    }
+
+    // 创建客户端，发送请求，返回结果
+    SetP::Response::SharedPtr call_set_parameters(const rcl_interfaces::msg::Parameter &param)
+    {
+        auto param_client = this->create_client<SetP>("turtle_controller/set_parameters");
+
+        // 1.检测服务端是否上线
+        while (!param_client->wait_for_service(1s)) {
+            if (!rclcpp::ok()) {
+                RCLCPP_ERROR(this->get_logger(), "等待服务过程中，rclcpp挂了，我退下了");
+                return nullptr;
+            }
+            RCLCPP_INFO(this->get_logger(), "等待服务上线中....");
+        }
+
+        // 2.构建请求对象
+        auto request = std::make_shared<SetP::Request>();
+        request->parameters.push_back(param);
+
+        // 3.发送请求（同步等待结果）
+        auto future = param_client->async_send_request(request);
+        rclcpp::spin_until_future_complete(this->get_node_base_interface(), future);
+        auto response = future.get();
+        return response;
+    }
+
+    // 更新参数k
+    void update_server_param_k(double k)
+    {
+        auto param = rcl_interfaces::msg::Parameter();
+        param.name = "k";
+
+        auto param_value = rcl_interfaces::msg::ParameterValue();
+        param_value.type = rcl_interfaces::msg::ParameterType::PARAMETER_DOUBLE;
+        param_value.double_value = k;
+        param.value = param_value;
+
+        auto response = this->call_set_parameters(param);
+        if (response == nullptr) {
+            RCLCPP_INFO(this->get_logger(), "参数更新失败");
+            return;
+        }
+
+        for (auto &result : response->results) {
+            if (!result.successful) {
+                RCLCPP_INFO(this->get_logger(), "参数更新失败，原因：%s", result.reason.c_str());
+            } else {
+                RCLCPP_INFO(this->get_logger(), "参数更新成功");
+            }
+        }
     }
 };
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    auto controller = std::make_shared<PatrolClient>();
-    rclcpp::spin(controller);
+    auto node = std::make_shared<PatrolClient>();
+    node->update_server_param_k(4.0);
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
